9bd0c77b34432e21a7d3b1cfa08be29740569842
Victor Hugo Rosa Fosneca <victor.vhrf@gmail.com>
2023-10-18 17:55:15 -0300

    feat: add fomr control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
-  filteredResults = [];
+export class FormsComponent implements OnInit {
+  filteredResults = []
+  formGroup!: FormGroup
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit() {
+    console.log(this.formGroup.value)
+  }
+
+  onClean() {
+    this.formGroup.reset()
+  }
 }

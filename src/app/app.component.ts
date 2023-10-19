78e2f5c2ba8a4c7a99fda1317294b44c364c93e1
Victor Hugo Rosa Fosneca <victor.vhrf@gmail.com>
2023-10-19 11:52:11 -0300

    feat: moved filter and card list to gert unit service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from 'src/app/types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-smartfit-challenge';
+  showList = new BehaviorSubject(false)
+  unitsList: Location[] = []
+
+  constructor(private unitServce: GetUnitsService) { }
+
+  onSubmit() {
+    this.unitsList = this.unitServce.getFilteredUnits()
+    this.showList.next(true)
+  }
 }
